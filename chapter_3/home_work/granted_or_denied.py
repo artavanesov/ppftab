f11d89ed89afd7b0eb7eb564463335520ae42ed1
artavanesov <artyom.a.avanesov@gmail.com>
2018-07-04 14:01:44 +0300

    Chapter 3: Started

---
+++ chapter_3/home_work/granted_or_denied.py
@@ -0,0 +1,14 @@
+# Granted of Denied
+# Demonstrates an else statement
+
+print("Welcome to System Security Inc.")
+print("-- where sequrity is our middle name\n")
+
+password = input("Enter your password: ")
+
+if password == "secret":
+    print("Access Granted")
+else:
+    print("Access Denied")
+
+input("\n\nPress the Enter key to exit.")
---
+++ chapter_3/password.py
@@ -0,0 +1,13 @@
+# Password
+# Demonstrates the if statement
+
+print("Welcome to System Security Inc.")
+print("-- where sequrity is our middle name\n")
+
+password = input("Enter your password: ")
+
+if password == "secret":
+    print("Access Granted")
+    print("Welcome! You must be someone very important.")
+
+input("\n\nPress the Enter key to exit.")
